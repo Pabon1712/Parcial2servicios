ebe88e33b028ff32a7b58086ec11f9c54c33f64a
santiago pabon <spabon1712@gmail.com>
2024-05-04 22:42:37 -0500

    parte 3 parcial

--- MicroservicioUsuarios/src/main/java/com/pabon/usuario/controller/AlumnoController.java
+++ MicroservicioUsuarios/src/main/java/com/pabon/usuario/controller/AlumnoController.java
@@ -1,83 +1,52 @@
 package com.pabon.usuario.controller;
 
-
-import com.pabon.usuario.entity.Alumno;
-import com.pabon.usuario.service.AlumnoService;
-import lombok.AllArgsConstructor;
-import org.apache.coyote.Response;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 
-@AllArgsConstructor
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.pabon.commons.controller.CommonController;
+import com.pabon.usuario.entity.Alumno;
+import com.pabon.usuario.service.AlumnoService;
+
 @RestController
-public class AlumnoController {
+public class AlumnoController extends CommonController <Alumno, AlumnoService>{
 
-    @Autowired
-    AlumnoService service;
-    
-    @Value("${config.balanceador.test}")
-    private String balanceadorTest;
-    
-    @GetMapping("/balanceador-test")
-    public ResponseEntity<?> balanceadorTest(){
-    	Map<String, Object> response= new HashMap<String, Object>();
-    	response.put("balanceador", balanceadorTest);
-    	response.put("alumno", service.findAll());
-    	
-    	return ResponseEntity.ok().body(response);
-    			
-    			
+	@Value("${config.balanceador.test}")
+	private String balanceadorTest;
+	
+	@GetMapping("/balanceador-test")
+	public ResponseEntity<?> balanceadorTest() {
+		Map<String, Object> response = new HashMap<String, Object>();
+		response.put("balanceador", balanceadorTest);
+		response.put("alumno", service.findAll());
+		
+		return ResponseEntity.ok().body(response);
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<?> editar(@RequestBody Alumno alumno, @PathVariable Long id) {
         
-    }
-    
-    @GetMapping("/getAll")
-    public ResponseEntity<?> listarAlumno(){
-        return ResponseEntity.ok().body(service.findAll());
-    }
+		Optional<Alumno> ob = service.findById(id);
 
-    @GetMapping("/getById/{id}")
-    public ResponseEntity<?> ver(@PathVariable Long id ){
-        Optional<Alumno> ob = service.findById(id);
-
-        if( ob.isEmpty() ){
+        if (ob.isEmpty()) {
             return ResponseEntity.noContent().build();
         }
-        return ResponseEntity.ok().body(ob.get());
-    }
 
-
-    @PostMapping("/crearAlumno")
-    public ResponseEntity<?> crear(@RequestBody Alumno alumno ){
-        Alumno alumnoDB = service.save(alumno);
-
-        return ResponseEntity.status(HttpStatus.CREATED ).body( alumnoDB );
-    }
-
-    @PutMapping("/{id}")
-    public ResponseEntity<?> editar(@RequestBody Alumno alumno, @PathVariable Long id){
-        Optional<Alumno> ob = service.findById( id );
-
-        if( ob.isEmpty()){
-            return ResponseEntity.noContent().build();
-        }
         Alumno alumnoBd = ob.get();
-        alumnoBd.setNombre( alumno.getNombre() );
-        alumnoBd.setApellido( alumno.getApellido() );
-        alumnoBd.setEmail( alumno.getEmail() );
+        alumnoBd.setNombre(alumno.getNombre());
+        alumnoBd.setApellido(alumno.getApellido());
+        alumnoBd.setEmail(alumno.getEmail());
 
-        return ResponseEntity.status(HttpStatus.CREATED).body( service.save(alumnoBd) );
+        return ResponseEntity.status(HttpStatus.CREATED).body(service.save(alumnoBd));
     }
-
-    @DeleteMapping("/{id}")
-    public ResponseEntity<?> eliminar( @PathVariable Long id ){
-        service.deleteById(id);
-        return ResponseEntity.noContent().build();
-    }
+	
 }
--- MicroservicioUsuarios/src/main/java/com/pabon/usuario/service/AlumnoService.java
+++ MicroservicioUsuarios/src/main/java/com/pabon/usuario/service/AlumnoService.java
@@ -1,13 +1,11 @@
 package com.pabon.usuario.service;
 
+import com.pabon.commons.service.CommonService;
 import com.pabon.usuario.entity.Alumno;
 
-import java.util.Optional;
-
-public interface AlumnoService {
-
-    public Iterable<Alumno> findAll();
-    public Optional<Alumno> findById( Long id );
-    public Alumno save(Alumno alumno);
-    public void deleteById(Long id);
+public interface AlumnoService extends CommonService<Alumno>{
+	
+	public Alumno save(Alumno alumno);
+	
+	public void deleteById (Long Id);
 }
